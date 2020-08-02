516f01c0a2078563a0894fe9c94eccecf13692cf
Travis A. Everett <travis.a.everett@gmail.com>
2020-08-02 15:19:54 -0500

    try tracking performance instead?

--- test.sh
+++ test.sh
@@ -5,15 +5,17 @@ test(){
 	time_helper() { /usr/bin/time $RAND_FILE 2>&1 | tail -1 | awk '{print $1}'; }  # this just returns the real run time
 	echo $'#!/bin/sh\necho Hello' $RANDOM > $RAND_FILE && chmod a+x  $RAND_FILE;
 	echo "Testing $RAND_FILE";
-	sysctl security.mac.asp
+	#sysctl security.mac.asp
 	echo "execution time #1: $(time_helper) seconds";
-	sysctl security.mac.asp
+	#sysctl security.mac.asp
 	echo "execution time #2: $(time_helper) seconds";
-	sysctl security.mac.asp
+	#sysctl security.mac.asp
 }
 
 # start a log stream that should see these events and throw it into the background
 # log stream --debug | tee $1 &
+
+sudo powermetrics --samplers tasks --show-process-coalition --show-process-io --show-process-netstat --show-responsible-pid --show-usage-summary -i 0 | tee $1 &
 
 # should be a full round-trip + TLS
 test
