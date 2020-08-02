68058c93b74d76174854861aa855750018ca9938
Travis A. Everett <travis.a.everett@gmail.com>
2020-08-02 18:56:16 -0500

    swing 2 at powermetrics

--- test.sh
+++ test.sh
@@ -8,20 +8,21 @@ test(){
 	#sysctl security.mac.asp
 	echo "execution time #1: $(time_helper) seconds";
 	#sysctl security.mac.asp
-	echo "execution time #2: $(time_helper) seconds";
+	#echo "execution time #2: $(time_helper) seconds";
 	#sysctl security.mac.asp
 }
 
 # start a log stream that should see these events and throw it into the background
 # log stream --debug | tee $1 &
 
-sudo powermetrics --samplers tasks --show-process-coalition --show-process-io --show-process-netstat --show-responsible-pid --show-usage-summary -i 0 | tee $1 &
+sudo powermetrics -u $1 --samplers tasks --show-process-coalition --show-usage-summary -i 0 &
+trap "sudo kill -SIGHUP $!" EXIT
 
 # should be a full round-trip + TLS
 test
 
 # subsequent tests are still a full round trip, but no TLS
-for i in {1..10}; do
+for i in {1..10000}; do
 	test
 done
 
