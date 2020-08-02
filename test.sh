e7014e2bbdf5821ca4f2a0348c03b24f0fab2d54
Travis A. Everett <travis.a.everett@gmail.com>
2020-08-02 11:59:11 -0500

    rm sudo and process table; instrument all logs

--- test.sh
+++ test.sh
@@ -2,7 +2,7 @@
 test(){
 	# borrowed from https://news.ycombinator.com/item?id=23278258
 	RAND_FILE="/tmp/test-$RANDOM.sh";
-	time_helper() { /usr/bin/time sudo $RAND_FILE 2>&1 | tail -1 | awk '{print $1}'; }  # this just returns the real run time
+	time_helper() { /usr/bin/time $RAND_FILE 2>&1 | tail -1 | awk '{print $1}'; }  # this just returns the real run time
 	echo $'#!/bin/sh\necho Hello' $RANDOM > $RAND_FILE && chmod a+x  $RAND_FILE;
 	echo "Testing $RAND_FILE";
 	echo "execution time #1: $(time_helper) seconds";
@@ -10,7 +10,7 @@ test(){
 }
 
 # start a log stream that should see these events and throw it into the background
-log stream --debug --info --predicate 'process == "syspolicyd" AND subsystem == "com.apple.securityd" AND category == "gk"' &
+log stream --debug | tee $1 &
 
 # should be a full round-trip + TLS
 test
@@ -19,8 +19,6 @@ test
 for i in {1..10}; do
 	test
 done
-
-ps -AM
 
 # kill job?
 kill $!
