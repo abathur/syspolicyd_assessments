8fad0c40742351bd2d77d5c299406316cb6f5c68
Travis A. Everett <travis.a.everett@gmail.com>
2020-10-21 18:13:11 -0500

    rtd

--- de_kc.py
+++ de_kc.py
@@ -1,36 +1,21 @@
 #!/usr/bin/python3
 
-import struct
-import sys
+# borrowed (and simplified) from
+# https://github.com/Heisenberk/decode-kcpassword (MIT license)
 
-# Function to decrypt the kcpassword
-def decrypt_kcpassword():
-    key = [125, 137, 82, 35, 210, 188, 221, 234, 163, 185, 31]
-    length = len(key)
-    f = open(sys.argv[1], "rb")
-    byte = list(f.read())
-    f.close()
+key = [125, 137, 82, 35, 210, 188, 221, 234, 163, 185, 31]
+length = len(key)
+f = open("/etc/kcpassword", "rb")
+byte = list(f.read())
+f.close()
 
-    end = False
-    kcpassword = []
-    for i in range(len(byte)):
-        if byte[i] ^ key[i % length] == 0:
-            end = True
+end = False
+kcpassword = []
+for i in range(len(byte)):
+    if byte[i] ^ key[i % length] == 0:
+        end = True
 
-        if end == False:
-            kcpassword.append(str(chr(byte[i] ^ key[i % length])))
+    if end == False:
+        kcpassword.append(str(chr(byte[i] ^ key[i % length])))
 
-    print("".join(map(str, kcpassword)))
-
-
-# Function main
-def main():
-    if len(sys.argv) < 2:
-        print("usage : ./decode-kcpassword.py KCPASSWORD_PATH")
-        exit()
-    decrypt_kcpassword()
-
-
-# Call to main
-if __name__ == "__main__":
-    main()
+print("".join(map(str, kcpassword)))
