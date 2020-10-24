0c8c4e99d3985bf8560bbbb20c3b8fc2f8394109
Travis A. Everett <travis.a.everett@gmail.com>
2020-10-24 16:24:49 -0500

    DIY

---
+++ prep.sh
@@ -0,0 +1,7 @@
+set -x
+echo "NIX_SSL_CERT_FILE=/nix/var/nix/profiles/default/etc/ssl/certs/ca-bundle.crt" >> $GITHUB_ENV
+export NIX_SSL_CERT_FILE=/nix/var/nix/profiles/default/etc/ssl/certs/ca-bundle.crt
+sudo launchctl setenv NIX_SSL_CERT_FILE "$cert_file"
+echo "/nix/var/nix/profiles/per-user/$USER/profile/bin" >> $GITHUB_PATH || true
+echo "/nix/var/nix/profiles/default/bin" >> $GITHUB_PATH || true
+source $HOME/.nix-profile/etc/profile.d/nix.sh || true
